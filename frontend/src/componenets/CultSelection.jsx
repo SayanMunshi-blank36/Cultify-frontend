316f4413fce17b74fca902f4e9cee1fb70ff5cdf
Sayan <sayanmunshi36@gmail.com>
2023-03-19 19:09:21 +0530

    Selection Page Design

--- frontend/src/componenets/CultSelection.jsx
+++ frontend/src/componenets/CultSelection.jsx
@@ -1,42 +1,44 @@
 import React from "react";
-import logo from '../img/logo.jpg';
-import './CultSelection.css';
+import logo from "../img/logo.jpg";
+import "./CultSelection.css";
 
 function CultSelection() {
-    function postData(){
+  function postData() {}
 
-    }
-    
-    return (
-        <div className='cultSelection'>
-            <div className='heading'>
-                <img className='cultifyLogo' src={logo} alt="logo" />
-                <h1>Cultify </h1>
-            </div>
-            <p className='detail'>
-                Choose atleast 2 cults to continue
-            </p>
-            <div className="cults">
-                <div className="cult"> Cult one</div>
-                <div className="cult"> Cult seven</div>
-                <div className="cult"> Cult six</div>
-                <div className="cult"> Cult five</div>
-                <div className="cult"> Cult four</div>
-                <div className="cult"> Cult three</div>
-                <div className="cult"> Cult two</div>
-                <div className="cult"> Cult nine</div>
-                <div className="cult"> Cult one</div>
-                <div className="cult"> Cult seven</div>
-                <div className="cult"> Cult six</div>
-                <div className="cult"> Cult five</div>
-                <div className="cult"> Cult four</div>
-                <div className="cult"> Cult three</div>
-                <div className="cult"> Cult two</div>
-                <div className="cult"> Cult nine</div>
-            </div>
-            <button className="cultSelection-btn" onClick={postData()}>Continue</button>
-        </div>
-    );
+  return (
+    <div className="cultSelection">
+      <div className="heading">
+        <img className="cultifyLogo" src={logo} alt="logo" />
+        <h1>Cultify </h1>
+      </div>
+      <p className="detail">Choose atleast 2 cults to continue</p>
+      <div className="cults">
+        <div className="cult"> Cult one</div>
+        <div className="cult"> Cult seven</div>
+        <div className="cult"> Cult six</div>
+        <div className="cult"> Cult five</div>
+        <div className="cult"> Cult four</div>
+        <div className="cult"> Cult three</div>
+        <div className="cult"> Cult two</div>
+        <div className="cult"> Cult nine</div>
+        <div className="cult"> Cult one</div>
+        <div className="cult"> Cult seven</div>
+        <div className="cult"> Cult six</div>
+        <div className="cult"> Cult five</div>
+        <div className="cult"> Cult four</div>
+        <div className="cult"> Cult three</div>
+        <div className="cult"> Cult two</div>
+        <div className="cult"> Cult nine</div>
+        <div className="cult"> Cult four</div>
+        <div className="cult"> Cult three</div>
+        <div className="cult"> Cult two</div>
+        <div className="cult"> Cult nine</div>
+      </div>
+      <button className="cultSelection-btn" onClick={postData()}>
+        Continue
+      </button>
+    </div>
+  );
 }
 
 export default CultSelection;
