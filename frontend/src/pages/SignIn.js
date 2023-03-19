459cf6932733ab43f8268bc2cb5aa1ab8fa16682
Sayan <sayanmunshi36@gmail.com>
2023-03-19 14:05:59 +0530

    Sign Up and Login Screen

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import { useState } from "react";
+import "./SignUp.css";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../img/logo.jpg";
 
 function SignIn() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
-    <div className='homePage'>
-        <h1>SignIn</h1>
+    <div className="signUp">
+      <div className="form-container">
+        <div className="form">
+          <div className="heading">
+            <img className="cultifyLogo" src={logo} alt="logo" />
+            <h1>Cultify</h1>
+          </div>
+          <p className="signUpPara" style={{ color: "#fff" }}>
+            To Continue Login to Cultify.
+          </p>
+          <div style={{ width: "26rem" }}>
+            <label>Enter Email or Username</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Enter Your Email or Username"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div style={{ width: "26rem" }}>
+            <label>Enter Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <input type="submit" id="submit-btn" value="Login" />
+        </div>
+
+        <div className="form2">
+          Don't have an account ?
+          <Link to="/signup" id="lineline">
+            <span> Sign Up</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
