7201fd0fd57bcb5e23587d40affafbaf0938599b
Sayan <sayanmunshi36@gmail.com>
2023-03-20 02:07:49 +0530

    JWT Token check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
-import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import "./App.css";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Selection from "./pages/Selection";
 
 function App() {
+
   return (
     <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/selection" element={<Selection />} />
-            </Routes>
-        </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/selection" element={<Selection />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import Feed from '../componenets/Feed/Feed';
-import Navbar from '../componenets/Navbar/Navbar';
-import RightSeaction from '../componenets/RightSection/RightSeaction';
+import React, { useEffect } from "react";
+import Feed from "../componenets/Feed/Feed";
+import Navbar from "../componenets/Navbar/Navbar";
+import RightSeaction from "../componenets/RightSection/RightSeaction";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("jwt")) navigate("/signin");
+  }, []);
+
   return (
-    <div className='homePage'>
-        <Navbar/>
-        <div style = {{width : "100%", display : "flex", justifyContent : "space-between",}}>
-        <Feed/>
-        <RightSeaction/>
-        </div>
+    <div className="homePage">
+      <Navbar />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Feed />
+        <RightSeaction />
+      </div>
     </div>
   );
 }
