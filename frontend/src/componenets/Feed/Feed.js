988904aed7b15de89ae240e99bab1e451d8be4c7
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-03-20 10:33:13 +0530

    added files of post from backend

--- frontend/src/componenets/Feed/Feed.js
+++ frontend/src/componenets/Feed/Feed.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import styles from './Feed.module.css';
 import PostCard from './PostCard';
+import axios from 'axios';
 function Feed() {
+
+  const [allPost, setAllPost] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://cultify-backend-production.up.railway.app/api/post')
+      .then((response) => {
+        // console.log(response);
+        setAllPost(response.data.data);
+      })
+  }, []);
+
+  console.log(allPost);
   return (
     <div className={styles.container}>
-        <h2 className='styles.sectionHeader'>Latest Feed</h2>
-        <div className={styles.feedContainer}>
-            <PostCard/>
-        </div>
+      <h2 className='styles.sectionHeader'>Latest Feed</h2>
+      <div className={styles.feedContainer}>
+        {
+          allPost.map(post =>
+            // <PostCard description={post.description} key={post._id} handleLike={likePost} handleUnlike={unlikePost} imgsource={post.image} />
+            <PostCard description={post.description} key={post._id} imgsource={post.image} newid={post.cult} />
+          )
+        }
+        <PostCard />
+      </div>
     </div>
   )
 }
